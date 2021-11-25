062e70d26702c23bd2fb1d6d89401e34639734c2
ba4865-Robin Hauke <robin.hauke9@kit.edu>
2021-11-25 16:16:16 +0100

    added examples to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import praw
 import passwords as pwd
+import conversion as con
 
 def main():
     reddit = praw.Reddit(
@@ -7,6 +8,20 @@ def main():
         client_secret=pwd.reddit["client_secret"],
         user_agent="RedditUnitBot (by u/RedditUnitBot)",
     )
+
+    print(con.milestoKM(1))
+    print(con.kmtoMiles(1))
+    print(con.inchtoCm(1))
+    print(con.cmtoInch(1))
+    print(con.foottoCM(1))
+    print(con.cmtoFoot(1))
+    print(con.foottoMeter(1))
+    print(con.metertoFoot(1))
+    print(con.poundstoKg(1))
+    print(con.kilogrammtoPounds(1))
+    print(con.celsiustoFahrenheit(1))
+    print(con.fahrenheittoCelsius(1))
+
 
 
 if __name__ == "__main__":
