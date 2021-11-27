06f3d264d9de57d1e2a76b42174e64ee6517c0a4
franziskavonalbedyll <92818644+franziskavonalbedyll@users.noreply.github.com>
2021-11-27 23:35:04 +0100

    added example of basic usage

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import praw
-import passwords as pwd
+import reddit
+
 
 def main():
-    reddit = praw.Reddit(
-        client_id=pwd.reddit["client_id"],
-        client_secret=pwd.reddit["client_secret"],
-        user_agent="RedditUnitBot (by u/RedditUnitBot)",
-    )
+    subreddit = reddit.Subreddit("UnitBotTesting")
+    for post_id in subreddit.posts_ids:
+        post = reddit.Post(post_id)
+        print(post)
 
 
 if __name__ == "__main__":
